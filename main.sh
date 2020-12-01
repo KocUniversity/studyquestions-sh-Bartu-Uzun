a709a227ff89a73d1bc1843e1d94a00682883042
BartuUzun <buzun19@ku.edu.tr>
2020-12-01 17:21:42 +0000

    8. Soruya bak da mehhhh gerisi done

--- main.sh
+++ main.sh
@@ -1,1 +1,95 @@
-echo Hello World
+#echo Hello World
+### 8. SORUYA BAK DA MEHH
+
+### 1)
+################### DONE
+# echo "file1 is awesome" > file1.txt
+# cat < file1.txt >> output
+# cat < output >> file1.txt
+####################
+
+#### 2 ) 
+##############################
+#ANSWER is D)
+#head = listedeki ilk 10'u sırala
+# wc = sırasıyla : newline, word count, byte count printliyor
+###########################################
+
+### 3)
+####### DONE#####
+# 8G
+# d3w
+# G
+# $
+# 5p
+#######
+
+### 4)
+##### DONE!##########
+# v
+# L
+# 3e
+# y
+# $
+###########
+### 5)
+########### DONE
+
+# for directory in $(ls comp100_mt)
+# do
+#   echo "$directory"
+# done
+
+############3 DONE!#############
+
+#OR
+#ls -d */*/
+#PROBLEM:
+#comp100_mt/directory1/  'comp100_mt/directory 3/'
+#comp100_mt/directory2/   comp100_mt/directory4/
+
+
+#OR
+#find . -path '*/comp100_mt/* type -d'
+#PROBLEM :
+### GIVES PATH AS WELL
+# ./comp100_mt/directory1
+# ./comp100_mt/directory2
+# ./comp100_mt/directory 3
+# ./comp100_mt/directory4
+
+
+############# SORSORSRO
+
+
+### 6)
+################ DONE
+#mkdir -p comp100
+#########
+
+### 7)
+################# DONE
+#bash --version
+#####
+
+### 8)
+##########SORSORSOR
+
+
+### 9)
+#########3 DONE
+#rim -r test
+##############
+
+### 10)
+########## DONE
+# /<text> + n """for further occurances"""
+# /<text> + N """for previous occurances"""
+############################
+
+### 11)
+########### DONE
+# cut: d
+# copy: y
+# paste: p
+###########################
